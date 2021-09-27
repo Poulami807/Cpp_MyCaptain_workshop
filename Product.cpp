94a01cd2fe1d8e4a2b8cde869e2c18e2d342162b
Poulami Ganguly <66109793+Poulami807@users.noreply.github.com>
2021-09-27 11:01:55 +0530

    Program 2

---
+++ Product.cpp
@@ -0,0 +1,16 @@
+//Write a C++ Program to find the product of two numbers entered by the User.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    double num1,num2,product;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    product=num1*num2;
+    cout<<"Product is "<<product;
+    return 0;
+}
